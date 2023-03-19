e7b776677521d162c61e377bea01ccc6dfafbf15
rjohnsen <rogercbjohnsen@gmail.com>
2023-03-19 12:00:35 +0000

    Updates to streamlit gui. Added images folder

--- sentifender.py
+++ sentifender.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import json
 
+st. set_page_config(layout="wide")
+
 def load_schemas():
 	with open("datacollectors/output/schemas.json", "r") as schemas_file:
 		return json.load(schemas_file)
@@ -48,26 +50,32 @@ def get_pivots(selected_table):
 				if field["name"] != "Timestamp":
 					schema_fields.append(field["name"])
 
-			pivots.append({
-				"from": selected_table,
-				"to": schema["name"],
-				"via": list(set(source_fields).intersection(schema_fields)) 
-			})
+			via = list(set(source_fields).intersection(schema_fields))
+
+			if len(via) > 0:
+				pivots.append({
+					"from": selected_table,
+					"to": schema["name"],
+					"via": via 
+				})
 
 	return pivots
 
-st.title("Sentifender Lexica Detectica")
-table_names = get_table_names()
+with st.sidebar:
+	st.image("images/kusto-alt-icon-original.svg")
 
-selected_table = st.selectbox(
-	"Select table",
-	table_names
-)
+	st.title("Sentifender Lexica Detectica")
+	table_names = get_table_names()
+
+	selected_table = st.selectbox(
+		"Select table",
+		table_names
+	)
 
 schema_data = get_table_definition(selected_table)
 
 if schema_data is not False:
-	st.header(selected_table)
+	st.header(f"{selected_table} (table)")
 	st.subheader("Schema")
 	st.table(schema_data["fields"])
 
@@ -75,10 +83,17 @@ if schema_data is not False:
 	pivots = get_pivots(selected_table)
 
 	for pivot in pivots:
-		st.markdown(f"### {pivot['to']}")
+		st.markdown(f"#### {pivot['to']}")
 		st.table(pivot)
-		st.markdown("---")
 
+		st.write("The following Kusto query is machine generated and serves as a skeleton for further refinement by the analyst.")
+		variable_name = f"{pivot['via'][0].lower()}Var"
+		code_example = f'''
+let {variable_name}="";
+{selected_table}
+| where {pivot["via"][0]} == {variable_name}
+| join type=inner {pivot["to"]} on $left.{pivot["via"][0]} == $right.{pivot["via"][0]}'''
+		st.code(code_example, language="powershell")
 else:
 	st.write(f"Oh ... {selected_table} can't be found.")
 
